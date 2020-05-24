2109e4c54eaae3da3474a8556db201e1c73f6b9e
bobsi23 <sirakova99@abv.bg>
2020-05-24 21:21:35 +0300

    Add constructors getter for event

--- Event.h
+++ Event.h
@@ -1,5 +1,19 @@
 #pragma once
+#include <string>
 
 class Event {
+	string event_name;
 
+public:
+	Event() {
+		event_name = "";
+	}
+
+	Event(const string& _event_name) {
+		event_name = _event_name;
+	}
+
+	string get_event_name() const {
+		return event_name;
+	}
 };
