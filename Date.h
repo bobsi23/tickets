5536f8932fb1a1ed0cf4a51eb09cda30c9ccc164
bobsi23 <sirakova99@abv.bg>
2020-05-24 21:20:44 +0300

    Implement date as string

--- Date.h
+++ Date.h
@@ -1,10 +1,21 @@
 #pragma once
+#include <string>
+#include <fstream>
+using std::istream;
+using std::string;
 
-// TODO: Implement
 class Date {
-	
+	string date_as_string;
+
 public:
 	bool operator==(const Date& other) const {
-		return false;
+		return date_as_string == other.date_as_string;
 	}
+
+	friend istream& operator>>(istream& in, Date& date);
 };
+
+istream& operator>>(istream& in, Date& date) {
+	in >> date.date_as_string;
+	return in;
+}
