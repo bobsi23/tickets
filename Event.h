59d734b182577f9971888ba312fa3adb093fc4f9
bobsi23 <sirakova99@abv.bg>
2020-05-25 20:16:05 +0300

    Add matrix of seats in Event

--- Event.h
+++ Event.h
@@ -1,25 +1,26 @@
 #pragma once
 #include <string>
+#include <vector>
+#include "Seat.h"
+using std::vector;
+using std::string;
 
 class Event {
 	string event_name;
-	int all_seats;
-	int booked_seats;
 	Date date;
+	vector<vector<Seat>> rows;
 
 public:
 	Event() {
 		event_name = "";
-		all_seats = 0;
-		booked_seats = 0;
 		date = Date();
+		rows = vector<vector<Seat>>();
 	}
 
-	Event(const string& _event_name, int _all_seats, int _booked_seats, const Date& _date) {
+	Event(const string& _event_name, int number_of_rows, int number_of_colums, const Date& _date) {
 		event_name = _event_name;
-		all_seats = _all_seats;
-		booked_seats = _booked_seats;
 		date = _date;
+		rows = vector<vector<Seat>>(number_of_rows, vector<Seat>(number_of_colums, Seat()));
 	}
 
 	string get_event_name() const {
@@ -27,15 +28,29 @@ public:
 	}
 
 	int get_all_seats() const {
-		return all_seats;
+		if (rows.empty()) {
+			return 0;
+		}
+		else {
+			int number_of_columns = rows[0].size();
+			return rows.size() * number_of_columns;
+		}
 	}
 
 	int get_booked_seats() const {
-		return booked_seats;
+		int counter = 0;
+		for (int i = 0; i < rows.size(); ++i) {
+			for (int j = 0; j < rows[i].size(); ++j) {
+				if (rows[i][j].is_booked()) {
+					++counter;
+				}
+			}
+		}
+		return counter;
 	}
 
 	int get_free_seats() const {
-		return all_seats - booked_seats;
+		return get_all_seats() - get_booked_seats();
 	}
 
 	Date get_date() const {
