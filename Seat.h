17007f4c4a9bd25266917e5e4808717aad28b842
bobsi23 <sirakova99@abv.bg>
2020-05-28 15:41:08 +0300

    Add methods for buying

--- Seat.h
+++ Seat.h
@@ -1,28 +1,41 @@
 #pragma once
 #include<string>
+#include<cstdlib>
+#include<ctime>
 using std::string;
 
 class Seat {
 	bool booked;
 	string note;
+	int ticket_serial_number;
 	
+	// Credits: https://en.cppreference.com/w/cpp/numeric/random/srand
+	int random_serial_number() {
+		srand(time(nullptr));		// srand makes rand really generate random number
+									// time(nullptr) is the number of seconds since 1970
+		return rand() + 1;			// + 1 so that it's not 0
+	}
+
 public:
 	Seat() {
 		booked = false;
 		note = "";
+		ticket_serial_number = 0;
 	}
+
 
 	bool is_booked() const {
 		return booked;
 	}
 	
-	bool book() {
+	void book() {
 		booked = true;
 	}
 
-	bool unbook() {
+	void unbook() {
 		booked = false;
 	}
+
 
 	void set_note(const string& _note) {
 		note = _note;
@@ -30,5 +43,21 @@ public:
 
 	string get_note() const {
 		return note;
+	}
+
+
+	bool is_bought() const {
+		return ticket_serial_number != 0;
+	}
+
+	void buy() {
+		if (!is_bought()) {
+			book();
+			ticket_serial_number = random_serial_number();
+		}
+	}
+
+	int get_ticket_serial_number() const {
+		return ticket_serial_number;
 	}
 };
